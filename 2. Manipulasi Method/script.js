8e6241c1c9a395b690c685e91fd29f9d0d2c33fa
ProjectRavel <rafaelcodeid@gmail.com>
2024-06-14 01:22:45 +0700

    Menambahkan folder Manipalusi DOM

---
+++ 2. Manipulasi Method/script.js
@@ -0,0 +1,5 @@
+const judul = document.getElementById('judul');
+judul.innerHTML = "<i>Rafael Pandu Sumanti</i>";
+
+const sectionA = document.querySelector('section#a');
+sectionA.innerHTML = "Aku sayang Ghifa"
